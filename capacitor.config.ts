b9b424ef61d1d2bfeeae1ea0993a6024d1785937
eliayank <s160423089@student.ubaya.ac.id>
2025-09-30 13:49:41 +0700

    first commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'foodapp_c',
+  webDir: 'www'
+};
+
+export default config;
